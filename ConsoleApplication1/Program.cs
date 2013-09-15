29bbbc63f357335f75206627ce48b0e3c78376ad
Vincent Yang <mscsharp@gmail.com>
2013-09-15 13:46:05 +1000

    Demo for Display and DisplayName attribute and demonstrate Display takes the precedence of DisplayName

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -16,9 +16,9 @@ namespace ConsoleApplication1
             ModelMetadata bar = GetModelMetadata<Model>("Bar");
             ModelMetadata baz = GetModelMetadata<Model>("Baz");
 
-            Console.WriteLine("Foo: {0}",foo.IsReadOnly);
-            Console.WriteLine("Bar: {0}", bar.IsReadOnly);
-            Console.WriteLine("Baz: {0}", baz.IsReadOnly);
+            Console.WriteLine("Foo: {0}",foo.DisplayName?? "N/A");
+            Console.WriteLine("Bar: {0}", bar.DisplayName?? "N/A");
+            Console.WriteLine("Baz: {0}", baz.DisplayName?? "N/A");
         }
 
         static ModelMetadata GetModelMetadata<TModel>(string propertyName)
@@ -34,15 +34,14 @@ namespace ConsoleApplication1
 
     public class Model
     {
-        [ReadOnly(true)]
+        
         public string Foo { get; set; }
 
-        [Editable(true)]
-        [ReadOnly(true)]
+        [DisplayName("Bar")]
         public string Bar { get; set; }
         
-        [Editable(false)]
-        [ReadOnly(false)]
+        [DisplayName("baz")]
+        [Display(Name = "BAZ", ShortName= "BA")]
         public string Baz { get; set; }
     }
 }
